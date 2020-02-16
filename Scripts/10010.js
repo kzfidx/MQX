4e8dc50999d0ac156cf9746567a2581a6bde5679
kzfidx <50869006+kzfidx@users.noreply.github.com>
2020-02-17 00:47:59 +0800

    Update 10010.js

--- Scripts/10010.js
+++ Scripts/10010.js
@@ -109,7 +109,7 @@ function showmsg() {
 		const free = signinfo.info.dataList[0]
 		const flow = signinfo.info.dataList[1]
 		const voice = signinfo.info.dataList[2]
-		detail = `話費: ${free.number}${free.unit}, 已用: ${flow.number}${flow.unit}, 剩餘: ${voice.number}${voice.unit}`
+		detail = `話費: ${free.number}${free.unit}, \n已用: ${flow.number}${flow.unit}, \n剩餘: ${voice.number}${voice.unit}`
 	}
 
 	// 詳細信息
